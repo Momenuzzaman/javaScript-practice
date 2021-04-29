7b02c490e194ceaaeb7788d5b49e1fb20f673f27
mdemon420 <mdeamon4@gmail.com>
2021-04-30 01:58:26 +0600

    oerator add

---
+++ arthmeticAndAssingmentOperator.js
@@ -0,0 +1,18 @@
+// Arithmetic Operator => +, -, *, /, %(Modulus), **(Exponent), ++, --,
+// Assignment Operator => =,+=, -=, *=, /=, %=
+
+
+var x = 4;
+x += 7;
+console.log(x); 
+// x = x+7 
+
+var y =25 ;
+y-=20;
+console.log(y);
+// y = y-20
+
+var z=y;
+z *=x;
+console.log(z);
+// z=z*x
